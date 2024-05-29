10c4303c9a9246a42c2c46d2869fc29ce3f85116
Tegibar1983 <tbtf1991@gmail.com>
2024-05-29 00:02:19 +0000

    I18N

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,16 +1,16 @@
-#!/usr/bin/env  python3
+#!/usr/bin/env python3
 """A Basic Flask app with internationalization support.
 """
-
-from flask import Flask, render_template, request
 from flask_babel import Babel
+from flask import Flask, render_template, request
+
 
 class Config:
     """Represents a Flask Babel configuration.
     """
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
@@ -25,12 +25,13 @@ def get_locale() -> str:
     """
     queries = request.query_string.decode('utf-8').split('&')
     query_table = dict(map(
-        lambda x: (x if '=' in x else '{}='.format(x)).split('='), queries,
-        ))
+        lambda x: (x if '=' in x else '{}='.format(x)).split('='),
+        queries,
+    ))
     if 'locale' in query_table:
-        if query_table['locale'] in app.config['LANGUAGES']:
+        if query_table['locale'] in app.config["LANGUAGES"]:
             return query_table['locale']
-        return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route('/')
